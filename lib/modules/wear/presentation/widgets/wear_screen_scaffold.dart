b4d4c6ac856a423ee0858a32494408270637c5b5
Vanilla1999 <41183994+Vanilla1999@users.noreply.github.com>
2026-08-24 01:00:16 +0300

    refactor(wear): route home button through aggregate navigation

--- lib/modules/wear/presentation/widgets/wear_screen_scaffold.dart
+++ lib/modules/wear/presentation/widgets/wear_screen_scaffold.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:smart_glasses/modules/wear/presentation/screens/home/wear_home_confirm_screen.dart';
+import 'package:smart_glasses/modules/wear/application/wear_screen_id.dart';
+import 'package:smart_glasses/modules/wear/config/wear_dependencies.dart';
 import 'package:smart_glasses/modules/wear/presentation/widgets/home_button.dart';
 import 'package:smart_glasses/modules/wear/presentation/widgets/wear_scanner_status_indicator.dart';
 import 'package:smart_glasses/modules/wear/presentation/widgets/wear_position_indicator.dart';
@@ -69,7 +71,12 @@ class WearScreenScaffold extends ConsumerWidget {
                 padding: const EdgeInsets.all(4.5),
                 child: HomeButtonWear(
                   onTap: onHomeTap ??
-                      () => context.push(WearHomeConfirmScreen.route),
+                      () {
+                        unawaited(
+                          WearDependencies.I.wearFlowController
+                              .requestNavigation(WearScreenId.homeConfirm),
+                        );
+                      },
                 ),
               ),
             ),
